47c945d2adea4f9d3b1bf34ca59b365bf2ce612e
Alexandr Chernyakov <chernyakovit@gmail.com>
2020-04-29 00:14:07 +0300

    Delete hashtabfirstUniqueCharInStr.py

--- hashtabfirstUniqueCharInStr.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-class Solution:
-    def firstUniqChar(self, s: str) -> int:
-      hashTable = Counter(s)
-      for idx in range(len(s)):
-        if(hashTable[s[idx]] == 1):
-          return idx
-      return -1
-        
