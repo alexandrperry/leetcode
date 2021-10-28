818e8a4d85775334b75e923e6b02509a7fc1da0b
Alexandr Chernyakov <chernyakovit@gmail.com>
2021-10-29 01:59:43 +0300

    Create sortedSquares.js

---
+++ array/sortedSquares.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    const result = []
+    
+    for (let i = nums.length -1; i>=0; i--){
+        if (Math.abs(nums[left]) < Math.abs(nums[right])) {
+            result[i] = nums[right] ** 2
+            right--;
+        } else {
+            result[i] = nums[left] ** 2
+            left++;
+        }
+    }
+    
+    return result
+};
