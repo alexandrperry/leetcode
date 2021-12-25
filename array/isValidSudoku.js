9c8876227ebe17b369c0cb22e070d977cb5fca63
Alexandr Chernyakov <chernyakovit@gmail.com>
2021-12-26 00:56:14 +0200

    Create isValidSudoku.js

---
+++ array/isValidSudoku.js
@@ -0,0 +1,32 @@
+/**
+ * @param {Array<Array<number>>} board
+ * @return {boolean}
+ */
+const isValidSudoku = (board) => {
+  const seen = new Set();
+
+  for (let i = 0; i < 9; i++) {
+    for (let j = 0; j < 9; j++) {
+      const char = board[i][j];
+
+      if (char !== '.') {
+        if (
+          seen.has(`${char} in row ${i}`) ||
+          seen.has(`${char} in col ${j}`) ||
+          seen.has(`${char} in block ${Math.floor(i / 3)} - ${Math.floor(j / 3)}`)) {
+
+          return false
+        }
+
+      }
+
+
+      seen.add(`${char} in row ${i}`)
+      seen.add(`${char} in col ${j}`)
+      seen.add(`${char} in block ${Math.floor(i / 3)} - ${Math.floor(j / 3)}`)
+
+    }
+  }
+
+  return true
+}
