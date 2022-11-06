de1f8cdc35516405882b8bae0bc545661d368772
Alexandr Chernyakov <chernyakovit@gmail.com>
2022-11-07 01:35:49 +0200

    Create validParentheses.js

---
+++ string/validParentheses.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const stack = [];
+    
+    for(let i = 0; i < s.length; i++){
+        if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+            stack.push(s[i]);
+        }
+        else if (s[i] === ')' && stack.length !== 0 && stack[stack.length - 1] === '(') {
+            stack.pop();
+        } 
+        else if (s[i] === '}' && stack.length !== 0 && stack[stack.length - 1] === '{') {
+            stack.pop();
+        }
+        else if (s[i] === ']' && stack.length !== 0 && stack[stack.length - 1] === '[') {
+            stack.pop();
+        }
+        else {
+            return false;
+        }
+    }
+    return stack.length === 0
+};
