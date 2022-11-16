90d49fa7ecc14a6be9fecb25308a3ce42e067726
Alexandr Chernyakov <chernyakovit@gmail.com>
2022-11-17 00:53:26 +0200

    Update lowestCommonAncestorBinaryTree.js

--- tree/lowestCommonAncestorBinaryTree.js
+++ tree/lowestCommonAncestorBinaryTree.js
@@ -33,3 +33,19 @@ const findLCA = (root, p, q) => {
     
     return left || right || curr
 }
+
+// better for understanding 
+var lowestCommonAncestor = function(root, p, q) {
+    while(root){
+        if(p.val > root.val && q.val > root.val){
+            root = root.right
+        }
+        
+        else if(p.val < root.val && q.val < root.val){
+            root = root.left
+        }
+        else{
+            return root
+        }
+    }
+};
