332c4585a59c42270b9aa36516160fd6f6fd38e5
Alexandr Chernyakov <chernyakovit@gmail.com>
2022-11-17 20:11:55 +0200

    Update majorityElement.py

--- array/majorityElement.py
+++ array/majorityElement.py
@@ -3,4 +3,28 @@ class Solution:
     counts = collections.Counter(nums)
     return max(counts.keys(),key=counts.get)
       
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+//Boyer–Moore majority vote algorithm
+var majorityElement = function(nums) {
+    let candidate = nums[0];
+    let counter = 1;
+    
+    for (let i = 1; i < nums.length; i++){
+        if(counter === 0){
+            candidate = nums[i]
+        }
+        if(candidate === nums[i]){
+            counter++
+        }else{
+            counter--
+        }
+    }
+    
+    return candidate
+};
         
